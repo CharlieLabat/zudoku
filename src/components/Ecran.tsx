6c5b8170f799cb94af188f615cbe3d51c01d43d2
Charlie LABAT <charlielabat17@gmail.com>
2026-03-10 11:54:58 +0100

    Essai vérif colonne

--- src/components/Ecran.tsx
+++ src/components/Ecran.tsx
@@ -11,32 +11,53 @@ const Ecran = ({ grilleRef }: EcranProps) => {
   const grilleTest = [
     [1, n, n, n, n, n, n, 1, n],
     [n, 2, n, n, n, n, n, n, n],
-    [n, n, 3, n, n, n, n, n, n],
+    [n, n, 3, n, n, 5, n, n, n],
     [n, n, n, 4, n, n, n, n, 1],
     [n, n, n, n, 5, n, n, n, n],
-    [n, n, n, n, n, 6, n, n, n],
+    [n, 3, n, n, n, 6, n, n, n],
     [n, n, n, n, n, n, 7, n, n],
     [n, n, n, 1, n, n, n, 9, n],
     [n, n, n, n, n, n, n, n, 8],
   ];
   const grilleInv = grilleTest.toReversed();
-  console.log(grilleInv);
-  const agloTest = () => {
+  // console.log(grilleInv);
+
+  const nbOccuences = (nombre, liste) => {
+    // Vérification par occuence
+    const occuences = [];
+    let occuencesId = liste.indexOf(nombre);
+    while (occuencesId !== -1) {
+      occuences.push(occuencesId);
+      occuencesId = liste.indexOf(nombre, occuencesId + 1);
+    }
+    return occuences;
+  };
+
+  const grilleRetourne = () => {
+    const grilleRetourneBase = [];
     grilleTest.forEach((casse, ligne) => {
-      //console.log(casse);
-      const occuences = [];
-      let occuencesId = casse.indexOf(1);
-      while (occuencesId !== -1) {
-        occuences.push(occuencesId);
-        occuencesId = casse.indexOf(1, occuencesId + 1);
-      }
-      console.log("Il y a : ");
-      console.log(occuences.length);
-      console.log("d'1 dans la ligne " + ligne);
-      if (occuences.length > 1) {
-        console.error("Il y a trop de 1 sur cette ligne");
+      console.log(casse)
+      
+    
+    });
+    console.log(grilleRetourneBase);
+  };
+
+  const agloTest = () => {
+    // Vérification de ligne
+    grilleTest.forEach((casse, ligne) => {
+      for (let n = 1; n <= 9; n++) {
+        let occuence = nbOccuences(n, casse);
+
+        /*  console.log("Il y a : ");
+        console.log(occuence.length);
+        console.log("d'" + n + " dans la ligne " + ligne); */
+        if (occuence.length > 1) {
+          console.error("Il y a trop de " + n + " sur cette ligne");
+        }
       }
     });
+    grilleRetourne();
   };
   agloTest();
 
